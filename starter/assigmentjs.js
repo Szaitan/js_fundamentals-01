4dcb11059a89a1fc67fe2e2d434ae27983784975
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2024-10-09 18:31:20 +0200

    revision-02

---
+++ starter/assigmentjs.js
@@ -0,0 +1,10 @@
+const COUNTRY = "Poland"
+const CONTINET = "Europe"
+let population = 2000000
+let isIsland = false
+const LANGUAGE = "polish"
+
+console.log(isIsland)
+console.log(population)
+console.log(COUNTRY)
+console.log(LANGUAGE)
--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,11 @@
 let js = "amazing";
-if (js === "amazing") alert("javascript is amazing");
 console.log(40+8+23+10);
+let first_val = "bob"
+first_val = "Bober kurwa"
+console.log(first_val)
+console.log(first_val)
+console.log(first_val)
+
+// Constant variable like pi can be written with all upper letters to create constant variable
+const PI = 3.14
+
